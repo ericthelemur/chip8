f5ec40ac5435ff81f297025782d9704068235135
ericthelemur <owenconnors1@gmail.com>
2022-10-06 16:47:28 +0100

    Add frequency as cli argument

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,9 @@ fn main() {
     let cli = Cli::parse();
 
     let filename: &str = &cli.rom;
-    let mut state = interpreter::VMState::new(700);
+    let mut state = interpreter::VMState::new(cli.freq);
 
     let rom = std::fs::read(filename).expect("ROM file doesn't exist");
-
     state.load(&rom);
 
     chip8_base::run(state);
@@ -21,6 +20,9 @@ struct Cli {
     /// A CHIP-8 ROM to load into the interpreter
     #[clap(validator = rom_exists)]
     rom: String,
+    // Frequency to run the interpreter at
+    #[clap(action, default_value_t = 700)]
+    freq: u32,
 }
 
 fn rom_exists(f: &str) -> Result<(), &'static str> {
